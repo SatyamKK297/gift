78ca7146afc996d316d94f0de9d1269eae17e3e3
SatyamKK297 <153988319+SatyamKK297@users.noreply.github.com>
2025-04-11 17:03:58 +0530

    main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!', 'TO YOU', 'RUDRA'],
+			strings: [ 'HAPPY', 'BIRTHDAY!', 'RUDRA'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
